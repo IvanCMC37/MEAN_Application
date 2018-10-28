cf08a6be1c49077950bcfe35eb172c8e7a994346
Ivan Chan <s3534199@student.rmit.edu.au>
2018-10-28 23:48:12 +1100

    Added Stylus

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
-const express = require('express');
+const express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser =  require('body-parser');
 
 // set develop env
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const app = express();
 
+// function to get used by middleware
+function compile (str, path) {
+    return stylus(str).set('filename', path);
+}
+
 // set route for modules
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+
+// import logger
+app.use(logger('dev'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// configure the stylus middleware
+app.use(stylus.middleware(
+    {
+        src: __dirname + 'public',
+        compile: compile
+    }
+));
+
+app.use(express.static(__dirname + '/public'));
+
 
 // route for webpage
 app.get('*', function(req, res) {
