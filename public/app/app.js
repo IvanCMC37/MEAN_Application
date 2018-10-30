29a8e5908963d15ece57d304bb7e8f7fa2cc0146
Ivan Chan <s3534199@student.rmit.edu.au>
2018-10-30 20:05:00 +1100

    Finished authorization

--- public/app/app.js
+++ public/app/app.js
@@ -3,10 +3,27 @@ angular.module('app', ['ngResource', 'ngRoute']);
 
 // define clientside route
 angular.module('app').config(function($routeProvider, $locationProvider) {
+    var routeRoleChecks = {
+        admin: {auth: function(mvAuth) {
+          return mvAuth.authorizeCurrentUserForRoute('admin')
+        }}
+      }
+
     // turn on html5 mode for routing
     $locationProvider.html5Mode(true);
 
     // define route
     $routeProvider
-        .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl'});
+        .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
+        .when('/admin/users', { templateUrl: '/partials/admin/user-list', 
+        controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
+    });
 });
+
+angular.module('app').run(function($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection) {
+      if(rejection === 'not authorized') {
+        $location.path('/');
+      }
+    })
+})
